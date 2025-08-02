ece06aafe9787bbcc2bc0ada34f1b5bcbbdc170f
De Bock Benny <bdebock@protonmail.com>
2025-08-02 19:17:35 +0200

    Add mod template

---
+++ src/client/java/be/heimdal/praisesalmongod/PraiseSalmonGodClient.java
@@ -0,0 +1,10 @@
+package be.heimdal.praisesalmongod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PraiseSalmonGodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
